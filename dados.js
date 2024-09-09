99e16dc5a29e335fed044d63239b2ee612e68e7f
BcordeirOx <beatrizpereiracordeiro@uol.com.br>
2024-09-08 21:42:34 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+
+let dados = [
+  { 
+    titulo: "Naruto",
+    descricao: "Em um mundo onde quase todos possuem superpoderes, Izuku Midoriya sonha em se tornar um herói, apesar de não ter nenhuma habilidade. Quando uma chance inesperada surge, ele se matricula na prestigiada escola para heróis U.A. e embarca em uma jornada épica para se tornar o maior herói de todos os tempos.",
+    link: "https://myheroacademia.fandom.com/wiki/My_Hero_Academia_Wiki",
+    tags:"aldeia da folha"
+    },
+    { 
+      titulo: "Demon Slayer",
+      descricao : "Tanjiro Kamado, um jovem carvoeiro gentil, descobre que sua família foi massacrada por demônios e sua irmã mais nova, Nezuko, foi transformada em uma. Determinado a reverter a maldição de Nezuko e vingar sua família, Tanjiro se junta aos Caçadores de Demônios e embarca em uma jornada perigosa.",
+      link: "https://kimetsu-no-yaiba.fandom.com/wiki/Kimetsu_no_Yaiba_Wiki",
+      tags:" demonios"
+    },
+    { 
+      titulo: "One Piece",
+      descricao: "Luffy, um jovem pirata com o sonho de se tornar o Rei dos Piratas, embarca em uma grande aventura em busca do tesouro lendário, o One Piece. Junto de sua tripulação, ele navega pelos mares, enfrenta piratas, marinheiros e outras ameaças, desvendando os mistérios do Grand Line.",
+      link: "https://onepiece.fandom.com/wiki/One_Piece_Wiki",
+      tags:" Luffy pirata"
+    }
+]
